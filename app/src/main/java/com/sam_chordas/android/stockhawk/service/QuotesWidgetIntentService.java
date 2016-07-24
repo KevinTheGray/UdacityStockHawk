5b28cbadc6e68c24c55e9ed5eebc99fb75f4b224
Kevin Gray <Kevin@GoPosse.com>
2016-07-24 16:12:17 -0400

    printing data

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/QuotesWidgetIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/QuotesWidgetIntentService.java
@@ -2,10 +2,17 @@ package com.sam_chordas.android.stockhawk.service;
 
 import android.annotation.TargetApi;
 import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Build;
 import android.util.Log;
 import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+import com.sam_chordas.android.stockhawk.data.QuotesWidgetProvider;
 
 /**
  * Created by KG on 7/24/16.
@@ -18,7 +25,34 @@ public class QuotesWidgetIntentService extends IntentService {
 
   @Override
   protected void onHandleIntent(Intent intent) {
-    Log.d("Die", "Due");
+    // Retrieve all of the Today widget ids: these are the widgets we need to update
+    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+    int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this,
+      QuotesWidgetProvider.class));
+
+    // Get all the current quotes
+    Cursor data = getContentResolver().query(QuoteProvider.Quotes.CONTENT_URI,
+      new String[]{ QuoteColumns._ID, QuoteColumns.SYMBOL, QuoteColumns.BIDPRICE,
+        QuoteColumns.PERCENT_CHANGE, QuoteColumns.CHANGE, QuoteColumns.ISUP},
+      QuoteColumns.ISCURRENT + " = ?",
+      new String[]{"1"},
+      null);
+
+    if (data == null) {
+      return;
+    }
+
+    if (!data.moveToFirst()) {
+      data.close();
+      return;
+    }
+
+    while (data.moveToNext()) {
+      String symbol = data.getString(data.getColumnIndex(QuoteColumns.SYMBOL));
+      Log.d("Derp", symbol);
+    }
+
+    data.close();
   }
 
   @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
